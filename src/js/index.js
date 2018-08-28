69945539ba120fdf57f44998f04249006f2473c1
Kaybel <kblmrtnz@gmail.com>
2018-08-28 10:18:22 -0300

    links

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,23 @@
 #! /usr/bin/env node
-const markdownLinkExtractor = require('../js/linkExtrae').markdownLinkExtractor;
+const markdownLinkExtractor = require('../js/linkExtrae');
+// para guardar lo que la persona escribe 
+const path = require('path');
+// file system con mas opciones
 const fs = require('fs-extra');
 // importa la info del fyleSistem, permite ingresar al archivo readme
 const [, , ...args] = process.argv;
-// para guardar lo que la persona escribe 
-const path = require('path');
 // obtiene la ruta.
 let pathAbs = args[0];
+
+let take = ((ruta) => {
+  // utf es para que lo convierta de binario a lenguaje humano
+  // throw es de la documentacion para el error
+  // readFile lee el archivo en la ruta que le pase como parametro.
+  fs.readFile(`${ruta}`, 'utf-8', (err, data) => {
+    if (err) throw err;
+    markdownLinkExtractor(data);
+  });
+});
 
 if (!path.isAbsolute(pathAbs)) {
   let ubicPath = process.cwd(pathAbs);
@@ -15,14 +26,3 @@ if (!path.isAbsolute(pathAbs)) {
   // une las rutas
   take(joinPath);
 }
-
-const take = ((ruta) => {
-  // utf es para que lo convierta de binario a lenguaje humano
-  // throw es de la documentacion para el error
-  // readFile lee el archivo en la ruta que le pase como parametro.
-  fs.readFile(`${ruta}`, 'utf-8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-    markdownLinkExtractor(data);
-  });
-});
--- src/js/linkExtrae.js
+++ src/js/linkExtrae.js
@@ -1,6 +1,23 @@
 
 const Marked = require('marked');
 
+// nueva promesa
+function readFilePromise(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf-8', (error, data) => {
+      if (error) {
+        return reject(error);
+        // Sabemos que hay un error, así que rechazamos la promesa
+        // Si hay error, también nos aseguramos con return de no seguir ejecutando nada más en esta función
+      }
+
+      return resolve(data);
+      // En caso de que no haya error resolvemos la promesa con los datos que recibimos en el callback
+    });
+  });
+};
+
+// (process.argv + ${funcionquemuestresololinks})
 // Función necesaria para extraer los links usando marked
 // (tomada desde biblioteca del mismo nombre y modificada para el ejercicio)
 // Recibe texto en markdown y retorna sus links en un arreglo
@@ -33,7 +50,7 @@ function markdownLinkExtractor(markdown) {
     });
   };
   Marked(markdown, { renderer: renderer });
-
+  console.log(links);
   return links;
 };
 
