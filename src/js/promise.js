00b91a8ec5c09ac324f6fe0106adf04de84cfa1c
Kaybel <kblmrtnz@gmail.com>
2018-08-29 15:32:40 -0300

    adding comments in promise doc

--- src/js/promise.js
+++ src/js/promise.js
@@ -5,28 +5,36 @@ const fetch = require('node-fetch');
 const fs = require('fs-extra');
 
 function infoPath(filePath) {
+  // se crea la nueva promesa 
   return new Promise((resolve, reject) => {
+    // primero verifica si hay error o si existe el archivo, si no hay inconvenientes lo recorre buscando los links.
     fs.readFile(filePath, 'utf-8', (error, data) => {
       if (error) {
         return reject(error);
       }
+      // se extrae la info de la funcion indicada en el README.md para recorrer el arr links
       let links = markdownLinkExtractor(data);
+      // en el array promise se va a pushear la info de status.
       let promise = [];
+      // se recorre links para extraer el link
       links.forEach((url) => {
+        // se ingresa al href del link el cual posee la info del status del link y esta info se pushea a promise que es un array vacio
         promise.push(fetch(url.href)
           .then((response) => {
+            // se crea la key status y se le otorga la info extraida anteriormente como value
             url.status = response.status;
+            // se retorna url dado que ahora el mismo posee el status, si no se retorna no va a "actualizar" la info de status en url. 
             return url;
           })
+          // se crea el catch para que, si la url esta rota indique "fail"
           .catch((err) => {
             url.status = 'fail';
             return url;
           }));
       });
+      // promise all toma en cuenta todas las funciones y sus valores para devolver una promesa global.
       Promise.all(promise).then((values) => {
         resolve(links);
-      }).catch((err) => {
-
       });
     });
   });
