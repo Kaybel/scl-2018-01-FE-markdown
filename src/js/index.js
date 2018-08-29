0a78d22931df39ef68882974d974dcb1a6cf1390
Kaybel <kblmrtnz@gmail.com>
2018-08-29 15:21:31 -0300

    adding comments

--- src/js/index.js
+++ src/js/index.js
@@ -6,8 +6,9 @@ const path = require('path');
 // importa la info del fyleSistem, permite ingresar al archivo readme
 const [, , ...args] = process.argv;
 // obtiene la ruta.
+// los , , ... signifca que va a agrupar varia info, no una sola, plural ! 
 let pathAbs = args[0];
-
+// obtiene la ruta absoluta porque la inicializa en 0, / . 
 if (!path.isAbsolute(pathAbs)) {
   mdlinks();
 }
@@ -16,8 +17,10 @@ function mdlinks() {
   let ubicPath = process.cwd(pathAbs);
   // ubica la ruta ingresada en la terminal.
   let joinPath = path.join(ubicPath, pathAbs);
-  // une las rutas
+  // une las rutas que ingresa el usuario con la ruta absoluta de la terminal
   infoPath(joinPath).then((value) => {
+    // se crea la promesa llamada del js promise para que imprima la info en la terminal.
     console.log(value);
+    // la info.
   });
 };
